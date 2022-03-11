47e6945a3a8c4c3f20039fb94b0b81724749b712
Kenia Lizama-Gamez <ke.yulizama@gmail.com>
2022-03-11 12:23:21 -0500

    starter code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,25 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var numbers;
+var specialCharacters;
+var uppercase;
+var lowercase;
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+// 1. use prompts for user to select the password they want
+  
+  //    b. choose lowercase, uppercase, numeric, &/or special characters  
 
-}
+  // if the ok button is clicked, result will be true (boolean) ok (yes) cancel (no)
+
+  // 2. validate the input
+
+  // 3. password is generated to match criteria
+  
+  
+  // 4. display password to page or alert
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
